79aab441e06427a3778e9752c79c1eb7d5bb2315
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-20 01:39:42 -0500

    add second URL

--- v1.0/instatyper-userscript-1.0.user.js
+++ v1.0/instatyper-userscript-1.0.user.js
@@ -4,25 +4,32 @@
 // @version      1.0
 // @description  An instant typer for NitroType.
 // @author       Meow
-// @match        https://www.nitrotype.com/race
+// @match        https://*.nitrotype.com/race
+// @match        https://*.nitrotype.com/profile
 // ==/UserScript==
 
 (function () {
-  let loop = setInterval(function () {
-    let texts = document.querySelectorAll(".dash-letter"); // Gets all the letters to be typed.
-    if (!texts) return; // Stops if theres no letters.
-    if (document.querySelector(".dashShield")) return console.log("Still Starting..."); // Stops if the race hasnt started yet.
-    clearInterval(loop); // Stops this loop so it doesnt keep opening windows.
+  console.log("Loading InstaTyper...");
+  if (window.location.href.includes("/race")) {
+    let loop = setInterval(function () {
+      let texts = document.querySelectorAll(".dash-letter"); // Gets all the letters to be typed.
+      if (!texts) return; // Stops if theres no letters.
+      if (document.querySelector(".dashShield")) return console.log("Still Starting..."); // Stops if the race hasnt started yet.
+      clearInterval(loop); // Stops this loop so it doesnt keep opening windows.
 
-    let titleText = "";
-    texts.forEach((t) => {
-      titleText += t.innerText; // Add each letter to the text to be typed.
+      let titleText = "";
+      texts.forEach((t) => {
+        titleText += t.innerText; // Add each letter to the text to be typed.
+      });
+      titleText = titleText.replace(/ /g, " "); // Replaces all the Non Breaking Spaces they use with regular spaces.
+
+      let keyDelay = 0; // W.I.P. keypress delay setting.
+
+      let sent = window.open("", "", "height=1,width=1"); // Creates a popup window.
+      sent.document.title = `${titleText}$NT${keyDelay}$NT`; // Sets the title in a format readable by the AHK script.
     });
-    titleText = titleText.replace(/ /g, " "); // Replaces all the Non Breaking Spaces they use with regular spaces.
+  } else if (window.location.href.includes("/profile")) {
 
-    let keyDelay = 0; // W.I.P. keypress delay setting.
 
-    let sent = window.open("", "", "height=1,width=1"); // Creates a popup window.
-    sent.document.title = `${titleText}$NT${keyDelay}$NT`; // Sets the title in a format readable by the AHK script.
-  });
+  }
 })();
